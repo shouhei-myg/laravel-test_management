e97b63d7c2691c636e09a9fde8a8a6ce07907952
宮城翔平 <shohei@miyagisheinoair.lan>
2022-02-09 14:23:33 +0900

    migrate作成

---
+++ database/migrations/2022_02_09_141747_create_login_infomation_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('login_infomation', function (Blueprint $table) {
+            $table->id();
+            $table->integer('environment_id');
+            $table->string('login_name');
+            $table->string('login_password');
+            $table->timestamp('deleted_at')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login_infomation');
+    }
+};
